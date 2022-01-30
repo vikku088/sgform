efd99aec5aa225ddbce37b1897d185cbec5a7908
vikku088 <viveksharma490@gmail.com>
2022-01-31 00:37:24 +0530

    mail template & further functions

--- Observer/Userip.php
+++ Observer/Userip.php
@@ -9,18 +9,81 @@ namespace Syncitgroup\Sgform\Observer;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Filesystem;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Exception\FileSystemException;
+use Magento\Framework\Filesystem\Directory\WriteInterface;
 
 class Userip implements ObserverInterface
 {
+    /**
+     * @var DirectoryList
+     */
+    private $directoryList;
+
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    public function __construct(
+        \Syncitgroup\Sgform\Helper\Sgdata $sgHelper,
+        DirectoryList $directoryList,
+        Filesystem $filesystem
+    )
+    {
+        $this->_sgHelper = $sgHelper;
+        $this->directoryList = $directoryList;
+        $this->filesystem = $filesystem;
+    }
 
     public function execute(Observer $observer)
     {
         try {
-            
+            $userIp = $this->_sgHelper->getCurrentIp();
+            $this->createDemoTextFile($userIp);
         } catch (Exception $e) {
             echo $e->getMessage();
         }
 
         return $this;
+    }
+
+    /**
+     * create custom folder and write text file
+     *
+     * @return bool
+     */
+    public function createDemoTextFile($userIp)
+    {
+        $varDirectory = $this->filesystem->getDirectoryWrite(
+            DirectoryList::VAR_DIR
+        );
+        $varPath = $this->directoryList->getPath('var');
+        $fileName = 'userip.txt'; //textfile name
+        $path = $varPath . '/sgform/' . $fileName;
+
+        // Write Content
+        $this->write($varDirectory, $path, $userIp);
+    }
+
+    /**
+     * Write content to text file
+     *
+     * @param WriteInterface $writeDirectory
+     * @param $filePath
+     * @return bool
+     * @throws FileSystemException
+     */
+    public function write(WriteInterface $writeDirectory, string $filePath, $userIp)
+    {
+        $stream = $writeDirectory->openFile($filePath, 'a');
+        $stream->lock();
+        $fileData = "Last User Ip: ". $userIp."\n";
+        $stream->write($fileData);
+        $stream->unlock();
+        $stream->close();
+
+        return true;
     }
 }
